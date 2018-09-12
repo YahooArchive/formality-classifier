3ca477d7682a80cbc50f15db0111bd1170615d9f
Ben Pearson <ben.pearson@oath.com>
2018-09-12 12:54:01 -0500

    Update regress.sh

--- regress.sh
+++ regress.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright 2016, Yahoo
+# Licensed under the terms of the Apache 2.0 license. See the LICENSE file in the project root for terms.
 
 traindir=experiments/news
 testdir=demo/test
